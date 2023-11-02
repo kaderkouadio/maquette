0212f90570117509083af8399413813a527e3295
Marie-Grace-AYEPO <marieayepo@gmail.com>
2023-11-02 09:53:38 +0000

    ajout de modification

--- maquette/app.py
+++ maquette/app.py
@@ -1,15 +1,17 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, flash, request, redirect
 import pyodbc as odbc
 
-DSN = 'Driver={SQL Server};Server=DESKTOP-DU03FQL\\SQLEXPRESS;Database=Gestion_des_Magasins;Trusted_Connection=yes;'
+
+DSN = 'Driver={SQL Server};Server=DESKTOP-DU03FQL\\SQLEXPRESS;Database=Gestion_des_Magasins;'
 conn = odbc.connect(DSN)
 cursor = conn.cursor()
 # uid = <username>;
 # pwd = <password>;
-print(conn)
+
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY']='clés_flash'
 
 
 @app.route("/")
@@ -27,26 +29,96 @@ def page3():
     return render_template("page3.html")
 
 
-@app.route("/page4/", methods=["get", "post"])
+@app.route("/page4/", methods=["GET", "POST"])
 def page4():
-    if request.method=="POST":
-        nom = request.form.get("Nom")
-        adresse = request.form.get("Adresse")
-        telephone = request.form.get("Telephone")
-        email = request.form.get("email")
-        cursor.execute(
-            """
-    INSERT INTO Magasin (NomMagasin, Adresse, Telephone, Email)
-    VALUES (?, ?, ?, ?)
-    """,
-            (nom, adresse, telephone, email)
-        )
-    return render_template("page4.html")
+    if request.method == "POST":
+        Nom = request.form["Nom"]
+        Adresse = request.form["Adresse"]
+        Telephone = request.form["Telephone"]
+        email = request.form["email"]
 
+        DSN = 'Driver={SQL Server};Server=DESKTOP-DU03FQL\\SQLEXPRESS;Database=Gestion_des_Magasins;'
+        conn = odbc.connect(DSN)
+        cursor = conn.cursor()
+        cursor.execute('''
+            INSERT INTO Magasin (NomMagasin, Adresse, Telephone, Email)
+            VALUES (?, ?, ?, ?)
+          ''', (Nom, Adresse, Telephone, email))
+        conn.commit()
+        conn.close()
+        # flash("Votre Magasin a été enregistré avec succès !", 'info')
+        return redirect(url_for('pages5'))
+    data = ''
+    return render_template("page4.html", data=data)
 
-@app.route("/page5/")
-def page5():
-    return render_template("page5.html")
+@app.route("/pages5", methods=['GET','POST'])
+def pages5():
+    DSN = "Driver={SQL Server};Server=DESKTOP-DU03FQL\\SQLEXPRESS;Database=Gestion_des_Magasins;"
+    conn = odbc.connect(DSN)
+    cursor = conn.cursor()
+    cursor.execute("select * from Magasin")
+    data = cursor.fetchall()
+    conn.close()
+    return render_template("pages5.html", data = data)
+
+@app.route('/edit/<int:item_id>', methods=['GET', 'POST'])
+def edit(item_id):
+    item_id = int(item_id)
+
+    # Connexion à la base de données
+    conn = odbc.connect(DSN)
+
+    # Création d'un objet curseur
+    cursor = conn.cursor()
+
+    # Récupération des données du magasin depuis la base de données
+    cursor.execute('SELECT * FROM magasin WHERE IdMagasin = ?', (item_id,))
+    data = cursor.fetchone()
+
+    # Si la méthode de la requête est POST, mise à jour des données du magasin dans la base de données
+    if request.method == 'POST':
+        # Récupération des données du formulaire
+        Nom = request.form['Nom']
+        Adresse = request.form['Adresse']
+        Telephone = request.form['Telephone']
+        email = request.form['email']
+
+        # Mise à jour des données du magasin dans la base de données
+        cursor.execute('''
+            UPDATE Magasin
+            SET NomMagasin = ?, Adresse = ?, Telephone = ?, Email = ?
+            WHERE IdMagasin = ?
+        ''', (Nom, Adresse, Telephone, email, item_id))
+
+        # Validation des modifications dans la base de données
+        conn.commit()
+
+        # Fermeture de la connexion à la base de données
+        conn.close()
+
+        # Affichage d'un message de succès à l'utilisateur
+        flash(f'Le magasin numéro {item_id} a été modifié avec succès !', 'info')
+
+        # Redirection de l'utilisateur vers la page du magasin
+        return redirect(url_for('pages5'))
+
+    # Retour du modèle de formulaire du magasin
+    return render_template('page4.html', data=data)
+
+@app.route('/delete/<int:item_id>', methods=['GET', 'POST'])
+def delete(item_id):
+    item_id = int(item_id)
+
+    conn = odbc.connect(DSN)
+    cursor = conn.cursor()
+
+    cursor.execute('DELETE FROM Magasin WHERE IdMagasin = ?', (item_id,))
+
+    conn.commit()
+    conn.close()
+
+    flash(f'Le magasin numéro {item_id} a été supprimé avec succès !', 'info')
+    return redirect(url_for('pages5'))
 
 
 @app.route("/page6/")
